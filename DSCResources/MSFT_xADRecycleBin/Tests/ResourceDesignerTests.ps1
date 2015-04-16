41ed475bf127222686a1effd0c79d7380cbd8926
Ashley McGlone <ashley@mcglone.com>
2015-04-15 22:10:22 -0700

    Create ResourceDesignerTests.ps1
    This uses Test-xDscResource and Test-xDscSchema.  It does not use Pester.

---
+++ DSCResources/MSFT_xADRecycleBin/Tests/ResourceDesignerTests.ps1
@@ -0,0 +1,2 @@
+Test-xDscResource xADRecycleBin -Verbose
+Test-xDscSchema -Path "C:\Program Files\WindowsPowerShell\Modules\xActiveDirectory\DSCResources\MSFT_xADRecycleBin\MSFT_xADRecycleBin.schema.mof" -Verbose
