00fcd1332e07d60d0569aaa22b0aba0c9c3807e3
Ashley McGlone <ashley.mcglone@microsoft.com>
2015-04-16 20:40:10 -0700

    Update and rename ResourceDesignerTests.ps1 to ResourceDesigner.Tests.ps1
    Attempting to comply with Pester.  Let me know if anything else needs to change.

--- DSCResources/MSFT_xADRecycleBin/Tests/ResourceDesignerTests.ps1
+++
@@ -1,2 +0,0 @@
-Test-xDscResource xADRecycleBin -Verbose
-Test-xDscSchema -Path "C:\Program Files\WindowsPowerShell\Modules\xActiveDirectory\DSCResources\MSFT_xADRecycleBin\MSFT_xADRecycleBin.schema.mof" -Verbose
