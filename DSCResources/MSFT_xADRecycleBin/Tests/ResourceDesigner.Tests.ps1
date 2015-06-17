07b09efc505851ea6ad2f2e59d8c8d94a3eb366d
Karol Kaczmarek <KarolKaczmarek@users.noreply.github.com>
2015-06-17 10:32:18 -0700

    Resource designer test fixes for xADRecycleBin

--- DSCResources/MSFT_xADRecycleBin/Tests/ResourceDesigner.Tests.ps1
+++ DSCResources/MSFT_xADRecycleBin/Tests/ResourceDesigner.Tests.ps1
@@ -1,7 +1,13 @@
 Describe 'xADRecycleBin' {
     Context 'xDscResouceDesigner' {
         It 'Pass Test-xDscResource' {
+            $rootDirectory = $pwd.Path + "\..\..\..\..\"
+	        $oldPSModulePath = $env:PSModulePath
+	        $env:PSModulePath = $env:PSModulePath + ";" + $rootDirectory
+
+            Write-Host "psmodulepath: $env:PSModulePath"
             Test-xDscResource xADRecycleBin -Verbose | Should Be $True
+	        $env:PSModulePath = $oldPSModulePath
         }
 
         It 'Pass Test-xDscSchema' {
