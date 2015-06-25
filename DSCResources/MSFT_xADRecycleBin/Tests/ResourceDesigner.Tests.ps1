d602a1bc93c7c5bf07ee1fa579d6ab1332a3a154
Cory Wood <corydwood@users.noreply.github.com>
2015-06-25 08:56:18 -0500

    Update ResourceDesigner.Tests.ps1

--- DSCResources/MSFT_xADRecycleBin/Tests/ResourceDesigner.Tests.ps1
+++ DSCResources/MSFT_xADRecycleBin/Tests/ResourceDesigner.Tests.ps1
@@ -1,7 +1,11 @@
 Describe 'xADRecycleBin' {
     Context 'xDscResouceDesigner' {
         It 'Pass Test-xDscResource' {
-            Test-xDscResource -Name ".\DSCResources\MSFT_xADRecycleBin" -Verbose | Should Be $True
+            $rootDirectory = Split-Path $pwd.Path -Parent
+            $oldPSModulePath = $env:PSModulePath
+            $env:PSModulePath = $env:PSModulePath + ";" + $rootDirectory
+            Test-xDscResource xADRecycleBin -Verbose | Should Be $True
+            $env:PSModulePath = $oldPSModulePath
         }
 
         It 'Pass Test-xDscSchema' {
