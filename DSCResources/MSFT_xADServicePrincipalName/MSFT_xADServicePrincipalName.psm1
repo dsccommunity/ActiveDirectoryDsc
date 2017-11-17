5fb5e45f3f897c28119d76303ba3fbf9d6edbf45
Claudio Spizzi <claudio@spizzi.net>
2017-11-17 01:45:12 +0100

    Add xADServicePrincipalName resource

---
+++ DSCResources/MSFT_xADServicePrincipalName/MSFT_xADServicePrincipalName.psm1
@@ -0,0 +1,192 @@
+
+<#
+    .SYNOPSIS
+    Returns the current state of the specified service principal name.
+
+    .PARAMETER Ensure
+    Specify if the SPN should be added or removed.
+
+    .PARAMETER ServicePrincipalName
+    Specify the full service principal name.
+
+    .PARAMETER Account
+    Specify the target account.
+#>
+function Get-TargetResource
+{
+    [CmdletBinding()]
+    [OutputType([System.Collections.Hashtable])]
+    param
+    (
+        [Parameter()]
+        [ValidateSet('Present', 'Absent')]
+        [System.String]
+        $Ensure = 'Present',
+
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [System.String]
+        $ServicePrincipalName,
+
+        [Parameter()]
+        [AllowEmptyString()]
+        [System.String]
+        $Account = ''
+    )
+
+    $spnAccounts = Get-ADObject -Filter { ServicePrincipalName -eq $ServicePrincipalName } -Properties 'SamAccountName' |
+                       Select-Object -ExpandProperty 'SamAccountName'
+
+    if ($spnAccounts.Count -eq 0)
+    {
+        # No SPN found
+        $returnValue = @{
+            Ensure               = 'Absent'
+            ServicePrincipalName = $ServicePrincipalName
+            Account              = ''
+        }
+    }
+    elseif ($spnAccounts.Count -eq 1)
+    {
+        # Exactly one SPN found, return the account name
+        $returnValue = @{
+            Ensure               = 'Present'
+            ServicePrincipalName = $ServicePrincipalName
+            Account              = $spnAccounts
+        }
+    }
+    else
+    {
+        # More then one SPN found, return 
+        $returnValue = @{
+            Ensure               = 'Present'
+            ServicePrincipalName = $ServicePrincipalName
+            Account              = $spnAccounts -join ';'
+        }
+    }
+
+    return $returnValue
+}
+
+<#
+    .SYNOPSIS
+    Add or remove the service principal name.
+
+    .PARAMETER Ensure
+    Specify if the SPN should be added or removed.
+
+    .PARAMETER ServicePrincipalName
+    Specify the full service principal name.
+
+    .PARAMETER Account
+    Specify the target account.
+#>
+function Set-TargetResource
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter()]
+        [ValidateSet('Present', 'Absent')]
+        [System.String]
+        $Ensure = 'Present',
+
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [System.String]
+        $ServicePrincipalName,
+
+        [Parameter()]
+        [AllowEmptyString()]
+        [System.String]
+        $Account = ''
+    )
+
+    $spnAccounts = Get-ADObject -Filter { ServicePrincipalName -eq $ServicePrincipalName } -Properties 'SamAccountName' |
+                       Select-Object -ExpandProperty 'SamAccountName'
+
+    if ([String]::IsNullOrEmpty($Account) -or ($null -eq (Get-ADObject -Filter { SamAccountName -eq $Account })))
+    {
+        throw "AD object with SamAccountName = '$Account' not found!"
+    }
+
+    if ($Ensure -eq 'Present')
+    {
+        foreach ($spnAccount in $spnAccounts)
+        {
+            if ($spnAccount -ne $Account)
+            {
+                Get-ADObject -Filter { SamAccountName -eq $spnAccount } |
+                    Set-ADObject -Remove @{ ServicePrincipalName = $ServicePrincipalName }
+            }
+        }
+
+        if ($spnAccounts -notcontains $Account)
+        {
+            Get-ADObject -Filter { SamAccountName -eq $Account } |
+                Set-ADObject -Add @{ ServicePrincipalName = $ServicePrincipalName }
+        }
+    }
+
+    if ($Ensure -eq 'Absent')
+    {
+        foreach ($spnAccount in $spnAccounts)
+        {
+            Get-ADObject -Filter { SamAccountName -eq $spnAccount } |
+                Set-ADObject -Remove @{ ServicePrincipalName = $ServicePrincipalName }
+        }
+    }
+}
+
+<#
+    .SYNOPSIS
+    Tests the service principal name.
+
+    .PARAMETER Ensure
+    Specify if the SPN should be added or removed.
+
+    .PARAMETER ServicePrincipalName
+    Specify the full service principal name.
+
+    .PARAMETER Account
+    Specify the target account.
+#>
+function Test-TargetResource
+{
+    [CmdletBinding()]
+    [OutputType([System.Boolean])]
+    param
+    (
+        [Parameter()]
+        [ValidateSet('Present', 'Absent')]
+        [System.String]
+        $Ensure = 'Present',
+
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [System.String]
+        $ServicePrincipalName,
+
+        [Parameter()]
+        [AllowEmptyString()]
+        [System.String]
+        $Account = ''
+    )
+
+    [System.Boolean] $desiredConfigurationMatch = $true
+
+    $currentConfig = Get-TargetResource @PSBoundParameters
+
+    $desiredConfigurationMatch = $desiredConfigurationMatch -and
+                                 $currentConfig.Ensure -eq $Ensure
+
+    if ($Ensure -eq 'Present')
+    {
+        $desiredConfigurationMatch = $desiredConfigurationMatch -and
+                                     $currentConfig.Account -eq $Account
+    }
+
+    return $desiredConfigurationMatch
+}
+
+Export-ModuleMember -function *-TargetResource
---
+++ Tests/Unit/MSFT_xADServicePrincipalName.Tests.ps1
@@ -0,0 +1,191 @@
+$Global:DSCModuleName   = 'xActiveDirectory'
+$Global:DSCResourceName = 'MSFT_xADServicePrincipalName'
+
+#region HEADER
+[String] $moduleRoot = Split-Path -Parent (Split-Path -Parent (Split-Path -Parent $Script:MyInvocation.MyCommand.Path))
+Write-Host $moduleRoot -ForegroundColor Green;
+if ( (-not (Test-Path -Path (Join-Path -Path $moduleRoot -ChildPath 'DSCResource.Tests'))) -or `
+     (-not (Test-Path -Path (Join-Path -Path $moduleRoot -ChildPath 'DSCResource.Tests\TestHelper.psm1'))) )
+{
+    & git @('clone','https://github.com/PowerShell/DscResource.Tests.git',(Join-Path -Path $moduleRoot -ChildPath '\DSCResource.Tests\'))
+}
+
+Import-Module (Join-Path -Path $moduleRoot -ChildPath 'DSCResource.Tests\TestHelper.psm1') -Force
+$TestEnvironment = Initialize-TestEnvironment `
+    -DSCModuleName $Global:DSCModuleName `
+    -DSCResourceName $Global:DSCResourceName `
+    -TestType Unit
+#endregion
+
+
+# Begin Testing
+try
+{
+
+    #region Pester Tests
+
+    InModuleScope $Global:DSCResourceName {
+
+        #region Function Get-TargetResource
+        Describe "$($Global:DSCResourceName)\Get-TargetResource" {
+
+            $testDefaultParams = @{
+                ServicePrincipalName = 'HOST/demo'
+                Account              = ''
+            }
+
+            Context 'No SPN set' {
+
+                Mock Get-ADObject
+
+                It 'should return absent' {
+
+                    $result = Get-TargetResource @testDefaultParams
+
+                    $result.Ensure               | Should Be 'Absent'
+                    $result.ServicePrincipalName | Should Be 'HOST/demo'
+                    $result.Account              | Should Be ''
+                }
+            }
+
+            Context 'One SPN set' {
+
+                Mock Get-ADObject { [PSCustomObject] @{ SamAccountName = 'User' } }
+
+                It 'should return present with the correct account' {
+
+                    $result = Get-TargetResource @testDefaultParams
+
+                    $result.Ensure               | Should Be 'Present'
+                    $result.ServicePrincipalName | Should Be 'HOST/demo'
+                    $result.Account              | Should Be 'User'
+                }
+            }
+
+            Context 'Multiple SPN set' {
+
+                Mock Get-ADObject { [PSCustomObject] @{ SamAccountName = 'User' }, [PSCustomObject] @{ SamAccountName = 'Computer' } }
+
+                It 'should return present with the multiple accounts' {
+
+                    $result = Get-TargetResource @testDefaultParams
+
+                    $result.Ensure               | Should Be 'Present'
+                    $result.ServicePrincipalName | Should Be 'HOST/demo'
+                    $result.Account              | Should Be 'User;Computer'
+                }
+            }
+        }
+        #endregion
+
+        #region Function Test-TargetResource
+        Describe "$($Global:DSCResourceName)\Test-TargetResource" {
+
+            $testDefaultParams = @{
+                ServicePrincipalName = 'HOST/demo'
+                Account              = 'User'
+            }
+
+            Context 'No SPN set' {
+
+                Mock Get-ADObject
+
+                It 'should return false for present' {
+
+                    $result = Test-TargetResource -Ensure 'Present' @testDefaultParams
+                    $result | Should Be $false
+                }
+
+                It 'should return true for absent' {
+
+                    $result = Test-TargetResource -Ensure 'Absent' @testDefaultParams
+                    $result | Should Be $true
+                }
+            }
+
+            Context 'Correct SPN set' {
+
+                Mock Get-ADObject { [PSCustomObject] @{ SamAccountName = 'User' } }
+
+                It 'should return true for present' {
+
+                    $result = Test-TargetResource -Ensure 'Present' @testDefaultParams
+                    $result | Should Be $true
+                }
+
+                It 'should return false for absent' {
+
+                    $result = Test-TargetResource -Ensure 'Absent' @testDefaultParams
+                    $result | Should Be $false
+                }
+            }
+
+            Context 'Wrong SPN set' {
+
+                Mock Get-ADObject { [PSCustomObject] @{ SamAccountName = 'Computer' } }
+
+                It 'should return false for present' {
+
+                    $result = Test-TargetResource -Ensure 'Present' @testDefaultParams
+                    $result | Should Be $false
+                }
+
+                It 'should return false for absent' {
+
+                    $result = Test-TargetResource -Ensure 'Absent' @testDefaultParams
+                    $result | Should Be $false
+                }
+            }
+
+            Context 'Multiple SPN set' {
+
+                Mock Get-ADObject { [PSCustomObject] @{ SamAccountName = 'User' }, [PSCustomObject] @{ SamAccountName = 'Computer' } }
+
+                It 'should return false for present' {
+
+                    $result = Test-TargetResource -Ensure 'Present' @testDefaultParams
+                    $result | Should Be $false
+                }
+
+                It 'should return false for absent' {
+
+                    $result = Test-TargetResource -Ensure 'Absent' @testDefaultParams
+                    $result | Should Be $false
+                }
+            }
+
+        }
+        #endregion
+
+        #region Function Set-TargetResource
+        Describe "$($Global:DSCResourceName)\Set-TargetResource" {
+
+            $testDefaultParams = @{
+                Ensure               = 'Present'
+                ServicePrincipalName = 'HOST/demo'
+                Account              = 'User'
+            }
+
+            Context 'No SPN set' {
+
+                Mock Get-ADObject -ParameterFilter { $Filter -eq ([ScriptBlock]::Create(' ServicePrincipalName -eq $ServicePrincipalName ')) } { }
+                Mock Get-ADObject { [PSCustomObject] @{ SamAccountName = 'User' } }
+                Mock Set-ADObject
+
+                It 'should call the Set-ADObject' {
+
+                    $result = Set-TargetResource @testDefaultParams
+
+                    Assert-MockCalled Set-ADObject -Scope Context
+                }
+            }
+        }
+        #endregion
+    }
+}
+finally
+{
+    #region FOOTER
+    Restore-TestEnvironment -TestEnvironment $TestEnvironment
+    #endregion
+}
