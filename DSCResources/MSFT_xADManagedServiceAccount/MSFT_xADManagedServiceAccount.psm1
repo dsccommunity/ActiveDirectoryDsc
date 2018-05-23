e6dabf29d3baa17b62d34d1d42546ebf89d3a3fe
Andrew Wickham <andrew@awickham.com>
2018-05-23 09:24:10 -0400

    Moving common functions import to top of file

--- DSCResources/MSFT_xADManagedServiceAccount/MSFT_xADManagedServiceAccount.psm1
+++ DSCResources/MSFT_xADManagedServiceAccount/MSFT_xADManagedServiceAccount.psm1
@@ -1,3 +1,7 @@
+## Import the common AD functions
+$adCommonFunctions = Join-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -ChildPath '\MSFT_xADCommon\MSFT_xADCommon.ps1'
+. $adCommonFunctions
+
 # Localized messages
 data LocalizedData
 {
@@ -278,9 +282,5 @@ function Set-TargetResource
         }
     } #end catch
 } #end function Set-TargetResource
-
-## Import the common AD functions
-$adCommonFunctions = Join-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -ChildPath '\MSFT_xADCommon\MSFT_xADCommon.ps1'
-. $adCommonFunctions
 
 Export-ModuleMember -Function *-TargetResource
