2c8a0168e9fbd337d5ed9faeaed4f972897a989a
Jan-Hendrik Peters [MSFT] <nyanhp@users.noreply.github.com>
2018-06-12 21:01:15 +0200

    Added class stubs

---
+++ Tests/Unit/Stubs/Microsoft.DirectoryServices.Deployment.Types.cs
@@ -0,0 +1,28 @@
+namespace Microsoft.DirectoryServices.Deployment.Types
+{
+    public enum DomainMode
+    {
+        Win2008 = 3,
+
+        Win2008R2 = 4,
+        
+        Win2012 = 5,
+        
+        Win2012R2 = 6,
+        
+        WinThreshold = 7
+    }
+
+    public enum ForestMode
+    {
+        Win2008 = 3,
+
+        Win2008R2 = 4,
+        
+        Win2012 = 5,
+        
+        Win2012R2 = 6,
+        
+        WinThreshold = 7
+    }
+}
