12cf49076bb5c0731e55cfca565d797e2b13943b
Angel Lopez <anlop@microsoft.com>
2018-09-24 12:53:26 -0400

    Moved Get-LocalizeData to helper module

--- DSCResources/MSFT_xADReplicationSiteLink/MSFT_xADReplicationSiteLink.psm1
+++ DSCResources/MSFT_xADReplicationSiteLink/MSFT_xADReplicationSiteLink.psm1
@@ -1,3 +1,4 @@
+Import-Module -Name (Join-Path -Path (Split-Path -Path $PSScriptRoot) -ChildPath CommonResourceHelper.psm1)
 
 <#
     .SYNOPSIS
@@ -308,64 +309,6 @@ function Resolve-SiteLinkName
     $adSite = Get-ADReplicationSite -Identity $SiteName
 
     return $adSite.Name
-}
-
-<#
-    .SYNOPSIS
-        Retrieves the localized string data based on the machine's culture.
-        Falls back to en-US strings if the machine's culture is not supported.
-
-    .PARAMETER ResourceName
-        The name of the resource as it appears before '.strings.psd1' of the localized string file.
-        For example:
-            AuditPolicySubcategory: MSFT_AuditPolicySubcategory
-            AuditPolicyOption: MSFT_AuditPolicyOption
-
-#>
-function Get-LocalizedData
-{
-    [OutputType([String])]
-    [CmdletBinding()]
-    param
-    (
-        [Parameter(Mandatory = $true, ParameterSetName = 'resource')]
-        [ValidateNotNullOrEmpty()]
-        [System.String]
-        $ResourceName,
-
-        [Parameter(Mandatory = $true, ParameterSetName = 'helper')]
-        [ValidateNotNullOrEmpty()]
-        [System.String]
-        $HelperName
-    )
-    
-    # With the helper module just update the name and path variables as if it were a resource. 
-    if ($PSCmdlet.ParameterSetName -eq 'helper')
-    {
-        $resourceDirectory = $PSScriptRoot
-        $ResourceName = $HelperName
-    }
-    else
-    {
-        # Step up one additional level to build the correct path to the resource culture.
-        $resourceDirectory = Join-Path -Path ( Split-Path $PSScriptRoot -Parent ) `
-            -ChildPath $ResourceName
-    }
-
-    $localizedStringFileLocation = Join-Path -Path $resourceDirectory -ChildPath $PSUICulture
-
-    if (-not (Test-Path -Path $localizedStringFileLocation))
-    {
-        # Fallback to en-US
-        $localizedStringFileLocation = Join-Path -Path $resourceDirectory -ChildPath 'en-US'
-    }
-
-    Import-LocalizedData `
-        -BindingVariable 'localizedData' `
-        -FileName "$ResourceName.strings.psd1" `
-        -BaseDirectory $localizedStringFileLocation
-
-    return $localizedData
 }
 
 $script:localizedData = Get-LocalizedData -ResourceName 'MSFT_xADReplicationSiteLink'
