336c273350d81410a4d22a586aa776d551f0bb2c
Andrew Wickham <andrew@awickham.com>
2018-10-11 09:19:51 -0400

    Removed comment describing InModuleScope

--- Tests/Unit/MSFT_xADManagedServiceAccount.Tests.ps1
+++ Tests/Unit/MSFT_xADManagedServiceAccount.Tests.ps1
@@ -25,8 +25,6 @@ try
 {
     #region Pester Tests
 
-    # The InModuleScope command allows you to perform white-box unit testing on the internal
-    # (non-exported) code of a Script Module.
     InModuleScope $Global:DSCResourceName {
        #region Pester Test Initialization
         $testPresentParams = @{
