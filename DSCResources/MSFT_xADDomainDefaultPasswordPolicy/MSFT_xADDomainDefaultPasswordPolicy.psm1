e32d39e0d747195c98d2b39ebfbba31bc91c202a
Simon Heather <simonheather99@gmail.com>
2019-05-31 20:00:23 +0100

    Fix BOM

--- DSCResources/MSFT_xADDomainDefaultPasswordPolicy/MSFT_xADDomainDefaultPasswordPolicy.psm1
+++ DSCResources/MSFT_xADDomainDefaultPasswordPolicy/MSFT_xADDomainDefaultPasswordPolicy.psm1
@@ -1,4 +1,4 @@
-﻿## Import the common AD functions
+## Import the common AD functions
 $adCommonFunctions = Join-Path `
     -Path (Split-Path -Path $PSScriptRoot -Parent) `
     -ChildPath '\MSFT_xADCommon\MSFT_xADCommon.psm1'
--- DSCResources/MSFT_xWaitForADDomain/MSFT_xWaitForADDomain.psm1
+++ DSCResources/MSFT_xWaitForADDomain/MSFT_xWaitForADDomain.psm1
@@ -1,4 +1,4 @@
-﻿function Get-TargetResource
+function Get-TargetResource
 {
     [OutputType([System.Collections.Hashtable])]
     param
