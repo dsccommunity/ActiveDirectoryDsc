PowerShell/xActiveDirectory

Initial commit - RODC Support

DSCResources/MSFT_xADDomainController/en-US/MSFT_xADDomainController.strings.psd1
@@ -17,4 +17,7 @@ ConvertFrom-StringData @'
     ExpectedGlobalCatalogEnabled  = The domain controller does not contain a Global Catalog, but it was expected to have a Global Catalog.
     ExpectedGlobalCatalogDisabled = The domain controller have a Global Catalog, but it was expected to not have a Global Catalog.
     ExpectedDomainController      = Expected the node to be a domain controller, but did not get a domain controller object. (ADDC0017)
+    AllowedSyncAccountsMismatch   = There is a mismatch in AllowPasswordReplicationAccountName list. Got {0}, expected was {1}. (ADDC0018)
+    DenySyncAccountsMismatch      = There is a mismatch in DenyPasswordReplicationAccountName list. Got {0}, expected was {1}. (ADDC0019)
+    RODCMissingSite               = You have specified 'ReadonlyReplica', but did not provide a site name. (ADDC0020)
 '@

dsccommunity/ActiveDirectoryDsc

DSCResources/MSFT_xADDomainController/en-US/MSFT_xADDomainController.strings.psd1
@@ -17,4 +17,7 @@ ConvertFrom-StringData @'
     ExpectedDomainController      = Expected the node to be a domain controller, but did not get a domain controller object. (ADDC0017)
     ExpectedGlobalCatalogEnabled  = The domain controller does not contain a Global Catalog, but it was expected to have a Global Catalog. (ADDC0018)
     ExpectedGlobalCatalogDisabled = The domain controller have a Global Catalog, but it was expected to not have a Global Catalog. (ADDC0019)
+    AllowedSyncAccountsMismatch   = There is a mismatch in AllowPasswordReplicationAccountName list. Got {0}, expected was {1}. (ADDC0020)
+    DenySyncAccountsMismatch      = There is a mismatch in DenyPasswordReplicationAccountName list. Got {0}, expected was {1}. (ADDC0021)
+    RODCMissingSite               = You have specified 'ReadonlyReplica', but did not provide a site name. (ADDC0022)
 '@