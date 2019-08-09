0c4dac13614651bf45ca1724f1bc1f1ff4a200b5
Johan Ljunggren <johan.ljunggren@shamn.se>
2019-08-09 10:01:17 +0200

    Fix ADObjectPermissionEntry

--- Tests/Unit/MSFT_ADObjectPermissionEntry.Tests.ps1
+++ Tests/Unit/MSFT_ADObjectPermissionEntry.Tests.ps1
@@ -43,6 +43,9 @@ try
     Invoke-TestSetup
 
     InModuleScope $script:dscResourceName {
+        #Load the AD Module Stub, so we can mock the cmdlets, then load the AD types
+        Import-Module (Join-Path -Path $PSScriptRoot -ChildPath 'Stubs\ActiveDirectoryStub.psm1') -Force
+
         #region Pester Test Initialization
         $testDefaultParameters = @{
             Path                               = 'CN=PC01,CN=Computers,DC=contoso,DC=com'
