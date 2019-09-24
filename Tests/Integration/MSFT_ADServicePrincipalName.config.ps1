a946872552b633c704a0a4878c0df434d719ea28
Mark Lewis <mark.lewis@zatzy.co.uk>
2019-09-24 14:43:24 +0100

    Initial Config of Tests
    Creates, adds and remvoes SPN from a user then computer account

---
+++ Tests/Integration/MSFT_ADServicePrincipalName.config.ps1
@@ -0,0 +1,142 @@
+#region HEADER
+# Integration Test Config Template Version: 1.2.0
+#endregion
+
+$configFile = [System.IO.Path]::ChangeExtension($MyInvocation.MyCommand.Path, 'json')
+if (Test-Path -Path $configFile)
+{
+    <#
+        Allows reading the configuration data from a JSON file, for real testing
+        scenarios outside of the CI.
+    #>
+    $ConfigurationData = Get-Content -Path $configFile | ConvertFrom-Json
+}
+else
+{
+    $currentDomain = Get-ADDomain
+    $netBiosDomainName = $currentDomain.NetBIOSName
+    $domainDistinguishedName = $currentDomain.DistinguishedName
+
+    $ConfigurationData = @{
+        AllNodes = @(
+            @{
+                NodeName        = 'localhost'
+                CertificateFile = $env:DscPublicCertificatePath
+            }
+        )
+    }
+}
+
+<#
+    .DESCRIPTION
+        This configuration will add a Service Principal Name to a user account.
+#>
+Configuration MSFT_ADServicePrincipalName_AddUserServicePrincipalName_Config
+{
+    Import-DscResource -Module ActiveDirectoryDsc
+
+    Node $AllNodes.NodeName
+    {
+        ADServicePrincipalName 'Integration_Test'
+        {
+            ServicePrincipalName = 'MSSQLSvc/sql01.contoso.com:1433'
+            Account              = 'SQL01Svc'
+            Ensure               = 'Present'
+        }
+    }
+}
+
+<#
+    .DESCRIPTION
+        This configuration will add a second Service Principal Name to a user account.
+#>
+Configuration MSFT_ADServicePrincipalName_AddSecondUserServicePrincipalName_Config
+{
+    Import-DscResource -Module ActiveDirectoryDsc
+
+    Node $AllNodes.NodeName
+    {
+        ADServicePrincipalName 'Integration_Test'
+        {
+            ServicePrincipalName = 'MSSQLSvc/sql02.contoso.com:1433'
+            Account              = 'SQL01Svc'
+            Ensure               = 'Present'
+        }
+    }
+}
+
+<#
+    .DESCRIPTION
+        This configuration will add a Service Principal Name to a computer account.
+#>
+Configuration MSFT_ADServicePrincipalName_AddComputerServicePrincipalName_Config
+{
+    Import-DscResource -Module ActiveDirectoryDsc
+
+    Node $AllNodes.NodeName
+    {
+        ADServicePrincipalName 'Integration_Test'
+        {
+            ServicePrincipalName = 'HTTP/web.contoso.com'
+            Account              = 'IIS01$'
+            Ensure               = 'Present'
+        }
+    }
+}
+
+<#
+    .DESCRIPTION
+        This configuration will remove a Service Principal Name from a user account.
+#>
+Configuration MSFT_ADServicePrincipalName_RemoveUserServicePrincipalName_Config
+{
+    Import-DscResource -Module ActiveDirectoryDsc
+
+    Node $AllNodes.NodeName
+    {
+        ADServicePrincipalName 'Integration_Test'
+        {
+            ServicePrincipalName = 'MSSQLSvc/sql01.contoso.com:1433'
+            Account              = 'SQL01Svc'
+            Ensure               = 'Absent'
+        }
+    }
+}
+
+<#
+    .DESCRIPTION
+        This configuration will remove the second Service Principal Name from a user account.
+#>
+Configuration MSFT_ADServicePrincipalName_RemoveSecondUserServicePrincipalName_Config
+{
+    Import-DscResource -Module ActiveDirectoryDsc
+
+    Node $AllNodes.NodeName
+    {
+        ADServicePrincipalName 'Integration_Test'
+        {
+            ServicePrincipalName = 'MSSQLSvc/sql02.contoso.com:1433'
+            Account              = 'SQL01Svc'
+            Ensure               = 'Absent'
+        }
+    }
+}
+
+<#
+    .DESCRIPTION
+        This configuration will add a Service Principal Name to a computer account.
+#>
+Configuration MSFT_ADServicePrincipalName_RemoveComputerServicePrincipalName_Config
+{
+    Import-DscResource -Module ActiveDirectoryDsc
+
+    Node $AllNodes.NodeName
+    {
+        ADServicePrincipalName 'Integration_Test'
+        {
+            ServicePrincipalName = 'HTTP/web.contoso.com'
+            Account              = 'IIS01$'
+            Ensure               = 'Absent'
+        }
+    }
+}
