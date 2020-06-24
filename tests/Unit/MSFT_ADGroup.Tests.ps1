ae106a5aeb9105b362aa5c8373a554e05f2805aa
Jeremy Ciak <jeremy.ciak@aderant.com>
2020-06-24 12:58:58 -0400

    Adding ObjectSID attribute and mock scenario

--- tests/Unit/MSFT_ADGroup.Tests.ps1
+++ tests/Unit/MSFT_ADGroup.Tests.ps1
@@ -70,28 +70,32 @@ try
             ObjectGUID        = 'a97cc867-0c9e-4928-8387-0dba0c883b8e'
             SamAccountName    = 'USER1'
             SID               = 'S-1-5-21-1131554080-2861379300-292325817-1106'
+            ObjectSID         = 'S-1-5-21-1131554080-2861379300-292325817-1106'
         }
         $fakeADUser2 = [PSCustomObject] @{
             DistinguishedName = 'CN=User 2,CN=Users,DC=contoso,DC=com'
             ObjectGUID        = 'a97cc867-0c9e-4928-8387-0dba0c883b8f'
             SamAccountName    = 'USER2'
             SID               = 'S-1-5-21-1131554080-2861379300-292325817-1107'
+            ObjectSID         = 'S-1-5-21-1131554080-2861379300-292325817-1107'
         }
         $fakeADUser3 = [PSCustomObject] @{
             DistinguishedName = 'CN=User 3,CN=Users,DC=contoso,DC=com'
             ObjectGUID        = 'a97cc867-0c9e-4928-8387-0dba0c883b90'
             SamAccountName    = 'USER3'
             SID               = 'S-1-5-21-1131554080-2861379300-292325817-1108'
+            ObjectSID         = 'S-1-5-21-1131554080-2861379300-292325817-1108'
         }
         $fakeADUser4 = [PSCustomObject] @{
             DistinguishedName = 'CN=User 4,CN=Users,DC=sub,DC=contoso,DC=com'
             ObjectGUID        = 'ebafa34e-b020-40cd-8652-ee7286419869'
             SamAccountName    = 'USER4'
             SID               = 'S-1-5-21-1131554080-2861379300-292325817-1109'
+            ObjectSID         = 'S-1-5-21-1131554080-2861379300-292325817-1109'
         }
 
         $fakeADGroup['Members'] = @(
-            $fakeADUser1.DistinguishedName
+            $fakeADUser1.DistinguishedName,
             $fakeADUser2.DistinguishedName
         )
 
@@ -247,6 +251,62 @@ try
                 Get-TargetResource @testPresentParams -Credential $testCredentials
 
                 Assert-MockCalled -CommandName Get-ADGroup -ParameterFilter {
+                    $Credential -eq $testCredentials
+                } -Scope It
+            }
+
+            It "Calls 'Get-ADGroupMember' with 'Server' parameter when 'DomainController' specified" {
+                Mock -CommandName Get-ADGroup -MockWith {
+                    return $fakeADGroup
+                }
+
+                Mock -CommandName Get-ADGroupMember -ParameterFilter {
+                    $Server -eq $testDomainController
+                } -MockWith {
+                    return @(
+                        $fakeADUser1,
+                        $fakeADUser2
+                    )
+                }
+
+                Mock -CommandName Get-ADObject -MockWith {
+                    return @(
+                        $fakeADUser1,
+                        $fakeADUser2
+                    )
+                }
+
+                Get-TargetResource @testPresentParams -DomainController $testDomainController
+
+                Assert-MockCalled -CommandName Get-ADGroupMember -ParameterFilter {
+                    $Server -eq $testDomainController
+                } -Scope It
+            }
+
+            It "Calls 'Get-ADGroupMember' with 'Credential' parameter when specified" {
+                Mock -CommandName Get-ADGroup -MockWith {
+                    return $fakeADGroup
+                }
+
+                Mock -CommandName Get-ADGroupMember -ParameterFilter {
+                    $Credential -eq $testCredentials
+                } -MockWith {
+                    return @(
+                        $fakeADUser1,
+                        $fakeADUser2
+                    )
+                }
+
+                Mock -CommandName Get-ADObject -MockWith {
+                    return @(
+                        $fakeADUser1,
+                        $fakeADUser2
+                    )
+                }
+
+                Get-TargetResource @testPresentParams -Credential $testCredentials
+
+                Assert-MockCalled -CommandName Get-ADGroupMember -ParameterFilter {
                     $Credential -eq $testCredentials
                 } -Scope It
             }
