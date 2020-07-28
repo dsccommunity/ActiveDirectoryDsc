3942bd5d5eb5328155a4ab192cf3d8c6a7255ca5
fl0p3k <51800354+fl0p3k@users.noreply.github.com>
2020-07-27 17:52:54 -0700

    Unit test coverage update

--- Tests/Unit/MSFT_ADFineGrainedPasswordPolicy.Tests.ps1
+++ Tests/Unit/MSFT_ADFineGrainedPasswordPolicy.Tests.ps1
@@ -227,6 +227,17 @@ try
                 }
             }
 
+            Context 'When Get-ADFineGrainedPasswordPolicy cannot find the policy' {
+                BeforeAll {
+                    Mock -CommandName Get-ADFineGrainedPasswordPolicy `
+                        -MockWith { throw New-Object Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException }
+                }
+
+                It 'Should not throw' {
+                    { Get-TargetResource @getTargetResourceParametersPolicy } | Should -Not -Throw
+                }
+            }
+
             Context 'When Get-ADFineGrainedPasswordPolicySubject throws an unexpected error' {
                 BeforeAll {
                     Mock -CommandName Get-ADFineGrainedPasswordPolicySubject `
