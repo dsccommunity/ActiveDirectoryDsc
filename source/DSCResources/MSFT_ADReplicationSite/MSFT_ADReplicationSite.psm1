fad509556601aedebb3f16a7325f7baa7483f4d0
Chris Hill <chris.hill@mafint.org>
2025-05-13 18:07:22 +0100

    Spelling

--- source/DSCResources/MSFT_ADReplicationSite/MSFT_ADReplicationSite.psm1
+++ source/DSCResources/MSFT_ADReplicationSite/MSFT_ADReplicationSite.psm1
@@ -163,7 +163,7 @@ function Set-TargetResource
 
     .PARAMETER RenameDefaultFirstSiteName
         Specify if the Default-First-Site-Name should be renamed, if it exists.
-        Dafult value is 'false'.
+        Default value is 'false'.
 
     .PARAMETER Description
         Specifies a description of the object. This parameter sets the value of
