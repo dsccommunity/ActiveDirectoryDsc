ec6b71bff10638d5a07b2c1c2e4bb6d92d454073
Dan Hughes <2237515+dan-hughes@users.noreply.github.com>
2025-06-02 17:59:50 +0100

    Remove unused suppression

--- tests/Unit/MSFT_ADServicePrincipalName.Tests.ps1
+++ tests/Unit/MSFT_ADServicePrincipalName.Tests.ps1
@@ -1,6 +1,5 @@
 # Suppressing this rule because Script Analyzer does not understand Pester's syntax.
 [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
-[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingConvertToSecureStringWithPlainText', '')]
 param ()
 
 BeforeDiscovery {
